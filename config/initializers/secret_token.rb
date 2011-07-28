b831fa725ae9719091b5cf561c47e109e9137dff
Oleg Puzanov <puzanov@gmail.com>
2011-07-28 22:01:33 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mugugu::Application.config.secret_token = '97ef1a52a3685577dcbef02e1a95065bbf572b098d499906f6cec500d61fdc1c3c7fd7d9a8e7d01eb7d7ffb75baa80ddc49df3fd328a8590208d9f073babcff6'
